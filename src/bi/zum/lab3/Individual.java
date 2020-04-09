849ea2c9c1ee4fac859a97f3b7a995c60592d020
Terezie Hrubanova <terezie-h@seznam.cz>
2020-04-09 16:27:11 +0200

    initial commit

---
+++ src/bi/zum/lab3/Individual.java
@@ -0,0 +1,166 @@
+package bi.zum.lab3;
+
+import cz.cvut.fit.zum.api.ga.AbstractEvolution;
+import cz.cvut.fit.zum.api.ga.AbstractIndividual;
+import cz.cvut.fit.zum.data.StateSpace;
+import cz.cvut.fit.zum.util.Pair;
+
+
+/**
+ * @author Your Name
+ */
+public class Individual extends AbstractIndividual {
+
+    private double fitness = Double.NaN;
+    private AbstractEvolution evolution;
+    
+    
+    // @TODO Declare your genotype
+    
+
+    /**
+     * Creates a new individual
+     * 
+     * @param evolution The evolution object
+     * @param randomInit <code>true</code> if the individial should be
+     * initialized randomly (we do wish to initialize if we copy the individual)
+     */
+    public Individual(AbstractEvolution evolution, boolean randomInit) {
+        this.evolution = evolution;
+        
+        if(randomInit) {
+
+            // @TODO initialize individual
+            
+        }
+    }
+
+    @Override
+    public boolean isNodeSelected(int j) {
+        
+        
+        // @TODO Implement based on your individual's genotype
+        
+        
+        return false;
+    }
+
+    /**
+     * Evaluate the value of the fitness function for the individual. After
+     * the fitness is computed, the <code>getFitness</code> may be called
+     * repeatedly, saving computation time.
+     */
+    @Override
+    public void computeFitness() {
+        
+        
+        // @TODO: Implement fitness based on your implementation
+        // Hint: use the StateSpace object
+        
+        this.fitness = Double.NaN; // this is wrong!
+    }
+
+    /**
+     * Only return the computed fitness value
+     *
+     * @return value of fitness fucntion
+     */
+    @Override
+    public double getFitness() {
+        return this.fitness;
+    }
+
+    /**
+     * Does random changes in the individual's genotype, taking mutation
+     * probability into account.
+     * 
+     * @param mutationRate Probability of a bit being inverted, i.e. a node
+     * being added to/removed from the vertex cover.
+     */
+    @Override
+    public void mutate(double mutationRate) {
+        
+        
+        // TODO: Implement mutation of the genotype
+        
+        
+    }
+    
+    /**
+     * Crosses the current individual over with other individual given as a
+     * parameter, yielding a pair of offsprings.
+     * 
+     * @param other The other individual to be crossed over with
+     * @return A couple of offspring individuals
+     */
+    @Override
+    public Pair crossover(AbstractIndividual other) {
+
+        Pair<Individual,Individual> result = new Pair();
+
+        // @TODO implement your own crossover
+        result.a = null;
+        result.b = null;
+        
+        return result;
+    }
+
+    
+    /**
+     * When you are changing an individual (eg. at crossover) you probably don't
+     * want to affect the old one (you don't want to destruct it). So you have
+     * to implement "deep copy" of this object.
+     *
+     * @return identical individual
+     */
+    @Override
+    public Individual deepCopy() {
+        Individual newOne = new Individual(evolution, false);
+       
+
+        // TODO: at least you should copy your representation of search-space state
+
+        // for primitive types int, double, ...
+        // newOne.val = this.val;
+
+        // for objects (String, ...)
+        // for your own objects you have to implement clone (override original inherited from Objcet)
+        // newOne.infoObj = thi.infoObj.clone();
+
+        // for arrays and collections (ArrayList, int[], Node[]...)
+        /*
+         // new array of the same length
+         newOne.pole = new MyObjects[this.pole.length];		
+         // clone all items
+         for (int i = 0; i < this.pole.length; i++) {
+         newOne.pole[i] = this.pole[i].clone(); // object
+         // in case of array of primitive types - direct assign
+         //newOne.pole[i] = this.pole[i]; 
+         }
+         // for collections -> make new instance and clone in for/foreach cycle all members from old to new
+         */
+
+        newOne.fitness = this.fitness;
+        return newOne;
+    }
+
+    /**
+     * Return a string representation of the individual.
+     *
+     * @return The string representing this object.
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        
+        
+        /* TODO: implement own string representation, such as a comma-separated
+         * list of indices of nodes in the vertex cover
+         */
+        
+        
+        sb.append(super.toString());
+
+        return sb.toString();
+    }
+}
